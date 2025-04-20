089b5d473d4afaeac6d50de725a029abff0c0f43
Bernhard Nordén <86689476+BerraLeet@users.noreply.github.com>
2025-04-20 09:46:33 +0200

    Update mitm_output.py
    adding Responses to output

--- mitm_output.py
+++ mitm_output.py
@@ -3,4 +3,13 @@ from mitmproxy.net.http.http1.assemble import assemble_request, assemble_respons
 f = open('/home/vagrant/PhisherMan/proxy_output.txt', 'w')
 
 def response(flow):
-    f.write(assemble_request(flow.request).decode('utf-8')) # Ignoring Binary data
+    try:
+        f.write("=== REQUEST ===\n")
+        f.write(assemble_request(flow.request).decode('utf-8'))
+        f.write("\n")
+
+        f.write("=== RESPONSE ===\n")
+        f.write(assemble_response(flow.response).decode('utf-8'))
+        
+    except:
+        pass  # letting run if having problems
